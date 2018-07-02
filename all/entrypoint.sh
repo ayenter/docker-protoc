fcda82b57a6cee018bbc1fb2697830dc5afadf56
Alec-Yenter <Alec.Yenter@ibm.com>
2018-07-02 10:31:32 -0500

    simplifying

--- all/entrypoint.sh
+++ all/entrypoint.sh
@@ -1,200 +1,212 @@
 #!/bin/bash
 
-set -e
+protoc -I/usr/local/include -I. \
+  -I/go/src \
+  -I/go/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+  -Iwrigley \
+  --go_out=plugins=grpc:. \
+  `find wrigley/wrigley -maxdepth 1 -name "*.proto"`
 
-printUsage() {
-    echo "gen-proto generates grpc and protobuf @ Namely"
-    echo " "
-    echo "Usage: gen-proto -f my-service.proto -l go"
-    echo " "
-    echo "options:"
-    echo " -h, --help           Show help"
-    echo " -f FILE              The proto source file to generate"
-    echo " -d DIR               Scans the given directory for all proto files"
-    echo " -l LANGUAGE          The language to generate (${SUPPORTED_LANGUAGES[@]})"
-    echo " -o DIRECTORY         The output directory for generated files. Will be automatically created."
-    echo " -i includes          Extra includes"
-    echo " --with-gateway       Generate grpc-gateway files (experimental)."
 
-}
 
-GEN_GATEWAY=false
-SUPPORTED_LANGUAGES=("go" "ruby" "csharp" "java" "python" "objc" "php")
-GEN_DIR="./gen"
-EXTRA_INCLUDES=""
-OUT_DIR=""
 
-DOCS_TYPE=""
-DOCS_DIR="./docs"
-DOCS="--doc_out=$DOCS_DIR"
 
-while test $# -gt 0; do
-    case "$1" in
-        -h|--help)
-            printUsage
-            exit 0
-            ;;
-        -f)
-            shift
-            if test $# -gt 0; then
-                FILE=$1
-            else
-                echo "no input file specified"
-                exit 1
-            fi
-            shift
-            ;;
-        -d)
-            shift
-            if test $# -gt 0; then
-                PROTO_DIR=$1
-            else
-                echo "no directory specified"
-                exit 1
-            fi
-            shift
-            ;;
-        -l)
-            shift
-            if test $# -gt 0; then
-                GEN_LANG=$1
-            else
-                echo "no language specified"
-                exit 1
-            fi
-            shift
-            ;;
-        -o) shift
-            OUT_DIR=$1
-            shift
-            ;;
-        -i) shift
-            EXTRA_INCLUDES="-I$1"
-            shift
-            ;;
-        --with-gateway)
-            GEN_GATEWAY=true
-            shift
-            ;;
-        -m|--with-docs)
-            shift
-            DOCS_TYPE=$1
-            DOCS="$DOCS --doc_opt=$1,docs"
-            shift
-            ;;
-        *)
-            break
-            ;;
-    esac
-done
 
-if [[ -z $FILE && -z $PROTO_DIR ]]; then
-    echo "Error: You must specify a proto file or proto directory"
-    printUsage
-    exit 1
-fi
+# set -e
 
-if [[ ! -z $FILE && ! -z $PROTO_DIR ]]; then
-    echo "Error: You may specifiy a proto file or directory but not both"
-    printUsage
-    exit 1
-fi
+# printUsage() {
+#     echo "gen-proto generates grpc and protobuf @ Namely"
+#     echo " "
+#     echo "Usage: gen-proto -f my-service.proto -l go"
+#     echo " "
+#     echo "options:"
+#     echo " -h, --help           Show help"
+#     echo " -f FILE              The proto source file to generate"
+#     echo " -d DIR               Scans the given directory for all proto files"
+#     echo " -l LANGUAGE          The language to generate (${SUPPORTED_LANGUAGES[@]})"
+#     echo " -o DIRECTORY         The output directory for generated files. Will be automatically created."
+#     echo " -i includes          Extra includes"
+#     echo " --with-gateway       Generate grpc-gateway files (experimental)."
 
-if [ -z $GEN_LANG ]; then
-    echo "Error: You must specify a language: ${SUPPORTED_LANGUAGES[@]}"
-    printUsage
-    exit 1
-fi
+# }
 
-if [[ ! ${SUPPORTED_LANGUAGES[*]} =~ "$GEN_LANG" ]]; then
-    echo "Language $GEN_LANG is not supported. Please specify one of: ${SUPPORTED_LANGUAGES[@]}"
-    exit 1
-fi
+# GEN_GATEWAY=false
+# SUPPORTED_LANGUAGES=("go" "ruby" "csharp" "java" "python" "objc" "php")
+# GEN_DIR="./gen"
+# EXTRA_INCLUDES=""
+# OUT_DIR=""
 
-if [[ "$GEN_GATEWAY" == true && "$GEN_LANG" != "go" ]]; then
-  echo "Generating grpc-gateway is Go specific."
-  exit 1
-fi
+# DOCS_TYPE=""
+# DOCS_DIR="./docs"
+# DOCS="--doc_out=$DOCS_DIR"
 
-case $DOCS_TYPE in
-    "markdown")
-        DOCS="$DOCS.md"
-        ;;
-    *)
-        DOCS="$DOCS.$DOCS_TYPE"
-        ;;
-esac
+# while test $# -gt 0; do
+#     case "$1" in
+#         -h|--help)
+#             printUsage
+#             exit 0
+#             ;;
+#         -f)
+#             shift
+#             if test $# -gt 0; then
+#                 FILE=$1
+#             else
+#                 echo "no input file specified"
+#                 exit 1
+#             fi
+#             shift
+#             ;;
+#         -d)
+#             shift
+#             if test $# -gt 0; then
+#                 PROTO_DIR=$1
+#             else
+#                 echo "no directory specified"
+#                 exit 1
+#             fi
+#             shift
+#             ;;
+#         -l)
+#             shift
+#             if test $# -gt 0; then
+#                 GEN_LANG=$1
+#             else
+#                 echo "no language specified"
+#                 exit 1
+#             fi
+#             shift
+#             ;;
+#         -o) shift
+#             OUT_DIR=$1
+#             shift
+#             ;;
+#         -i) shift
+#             EXTRA_INCLUDES="-I$1"
+#             shift
+#             ;;
+#         --with-gateway)
+#             GEN_GATEWAY=true
+#             shift
+#             ;;
+#         -m|--with-docs)
+#             shift
+#             DOCS_TYPE=$1
+#             DOCS="$DOCS --doc_opt=$1,docs"
+#             shift
+#             ;;
+#         *)
+#             break
+#             ;;
+#     esac
+# done
 
-PLUGIN_LANG=$GEN_LANG
-if [ $PLUGIN_LANG == 'objc' ] ; then
-    PLUGIN_LANG='objective_c'
-fi
+# if [[ -z $FILE && -z $PROTO_DIR ]]; then
+#     echo "Error: You must specify a proto file or proto directory"
+#     printUsage
+#     exit 1
+# fi
 
-if [[ $OUT_DIR == '' ]]; then
-    if [[ $GEN_LANG == "python" ]]; then
-        # Python needs underscores to read the directory name.
-        OUT_DIR="${GEN_DIR}/pb_$GEN_LANG"
-    else
-        OUT_DIR="${GEN_DIR}/pb-$GEN_LANG"
-    fi
-fi
+# if [[ ! -z $FILE && ! -z $PROTO_DIR ]]; then
+#     echo "Error: You may specifiy a proto file or directory but not both"
+#     printUsage
+#     exit 1
+# fi
 
-echo "Generating $GEN_LANG files for ${FILE}${PROTO_DIR} in $OUT_DIR"
+# if [ -z $GEN_LANG ]; then
+#     echo "Error: You must specify a language: ${SUPPORTED_LANGUAGES[@]}"
+#     printUsage
+#     exit 1
+# fi
 
-if [[ ! -d $OUT_DIR ]]; then
-  mkdir -p $OUT_DIR
-fi
+# if [[ ! ${SUPPORTED_LANGUAGES[*]} =~ "$GEN_LANG" ]]; then
+#     echo "Language $GEN_LANG is not supported. Please specify one of: ${SUPPORTED_LANGUAGES[@]}"
+#     exit 1
+# fi
 
-if [[ ! -d $DOCS_DIR ]]; then
-  mkdir -p $DOCS_DIR
-fi
+# if [[ "$GEN_GATEWAY" == true && "$GEN_LANG" != "go" ]]; then
+#   echo "Generating grpc-gateway is Go specific."
+#   exit 1
+# fi
 
-# Python also needs __init__.py files in each directory to import.
-# If __init__.py files are needed at higher level directories (i.e.
-# directories above $OUT_DIR), it's the caller's responsibility to
-# create them.
-if [[ $GEN_LANG == "python" ]]; then
-    touch $GEN_DIR/__init__.py
-    find $OUT_DIR -type d | xargs -n1 -I '{}' touch '{}/__init__.py'
-fi
+# case $DOCS_TYPE in
+#     "markdown")
+#         DOCS="$DOCS.md"
+#         ;;
+#     *)
+#         DOCS="$DOCS.$DOCS_TYPE"
+#         ;;
+# esac
 
-GEN_STRING=''
-case $GEN_LANG in
-    "go") 
-        GEN_STRING="--go_out=plugins=grpc:$OUT_DIR"
-        ;;
-    "java")
-        GEN_STRING="--grpc_out=$OUT_DIR --${GEN_LANG}_out=$OUT_DIR --plugin=protoc-gen-grpc=`which protoc-gen-grpc-java`"
-        ;;
-    *)
-        GEN_STRING="--grpc_out=$OUT_DIR --${GEN_LANG}_out=$OUT_DIR --plugin=protoc-gen-grpc=`which grpc_${PLUGIN_LANG}_plugin`"
-        ;;
-esac
+# PLUGIN_LANG=$GEN_LANG
+# if [ $PLUGIN_LANG == 'objc' ] ; then
+#     PLUGIN_LANG='objective_c'
+# fi
 
-PROTO_INCLUDE="-I /usr/include/ \
-    -I /usr/local/include/ \
-    -I /go/src/github.com/grpc-ecosystem/grpc-gateway/ \
-    $EXTRA_INCLUDES"
+# if [[ $OUT_DIR == '' ]]; then
+#     if [[ $GEN_LANG == "python" ]]; then
+#         # Python needs underscores to read the directory name.
+#         OUT_DIR="${GEN_DIR}/pb_$GEN_LANG"
+#     else
+#         OUT_DIR="${GEN_DIR}/pb-$GEN_LANG"
+#     fi
+# fi
 
-if [ ! -z $PROTO_DIR ]; then
-    PROTO_INCLUDE="$PROTO_INCLUDE -I $PROTO_DIR"
-    PROTO_FILES=(`find ${PROTO_DIR} -maxdepth 1 -name "*.proto"`)
-else 
-    PROTO_INCLUDE="-I . $PROTO_INCLUDE"
-    PROTO_FILES=($FILE)
-fi
+# echo "Generating $GEN_LANG files for ${FILE}${PROTO_DIR} in $OUT_DIR"
 
-protoc $PROTO_INCLUDE \
-    $GEN_STRING \
-    $DOCS \
-    ${PROTO_FILES[@]}
+# if [[ ! -d $OUT_DIR ]]; then
+#   mkdir -p $OUT_DIR
+# fi
 
-if [ $GEN_GATEWAY = true ]; then
-    GATEWAY_DIR=${OUT_DIR}
-    mkdir -p ${GATEWAY_DIR}
+# if [[ ! -d $DOCS_DIR ]]; then
+#   mkdir -p $DOCS_DIR
+# fi
 
-    protoc $PROTO_INCLUDE \
-		--grpc-gateway_out=logtostderr=true:$GATEWAY_DIR ${PROTO_FILES[@]}
-    protoc $PROTO_INCLUDE  \
-		--swagger_out=logtostderr=true:$GATEWAY_DIR ${PROTO_FILES[@]}
-fi
+# # Python also needs __init__.py files in each directory to import.
+# # If __init__.py files are needed at higher level directories (i.e.
+# # directories above $OUT_DIR), it's the caller's responsibility to
+# # create them.
+# if [[ $GEN_LANG == "python" ]]; then
+#     touch $GEN_DIR/__init__.py
+#     find $OUT_DIR -type d | xargs -n1 -I '{}' touch '{}/__init__.py'
+# fi
+
+# GEN_STRING=''
+# case $GEN_LANG in
+#     "go") 
+#         GEN_STRING="--go_out=plugins=grpc:$OUT_DIR"
+#         ;;
+#     "java")
+#         GEN_STRING="--grpc_out=$OUT_DIR --${GEN_LANG}_out=$OUT_DIR --plugin=protoc-gen-grpc=`which protoc-gen-grpc-java`"
+#         ;;
+#     *)
+#         GEN_STRING="--grpc_out=$OUT_DIR --${GEN_LANG}_out=$OUT_DIR --plugin=protoc-gen-grpc=`which grpc_${PLUGIN_LANG}_plugin`"
+#         ;;
+# esac
+
+# PROTO_INCLUDE="-I /usr/include/ \
+#     -I /usr/local/include/ \
+#     -I /go/src/github.com/grpc-ecosystem/grpc-gateway/ \
+#     $EXTRA_INCLUDES"
+
+# if [ ! -z $PROTO_DIR ]; then
+#     PROTO_INCLUDE="$PROTO_INCLUDE -I $PROTO_DIR"
+#     PROTO_FILES=(`find ${PROTO_DIR} -maxdepth 1 -name "*.proto"`)
+# else 
+#     PROTO_INCLUDE="-I . $PROTO_INCLUDE"
+#     PROTO_FILES=($FILE)
+# fi
+
+# protoc $PROTO_INCLUDE \
+#     $GEN_STRING \
+#     $DOCS \
+#     ${PROTO_FILES[@]}
+
+# if [ $GEN_GATEWAY = true ]; then
+#     GATEWAY_DIR=${OUT_DIR}
+#     mkdir -p ${GATEWAY_DIR}
+
+#     protoc $PROTO_INCLUDE \
+# 		--grpc-gateway_out=logtostderr=true:$GATEWAY_DIR ${PROTO_FILES[@]}
+#     protoc $PROTO_INCLUDE  \
+# 		--swagger_out=logtostderr=true:$GATEWAY_DIR ${PROTO_FILES[@]}
+# fi
